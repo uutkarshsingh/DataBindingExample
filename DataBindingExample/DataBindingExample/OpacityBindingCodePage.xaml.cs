f4ddbf1727f3b3426820168c13b52fdc3d3af7d7
utkarshSinghRathor <uutkarshsingh@gmail.com>
2016-11-14 15:25:53 +0530

    Added the page

---
+++ DataBindingExample/DataBindingExample/OpacityBindingCodePage.xaml.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DataBindingExample
+{
+	public partial class OpacityBindingCodePage : ContentPage
+	{
+		public OpacityBindingCodePage()
+		{
+			Label label = new Label
+			{
+				Text = "Opacicty Binding Demo",
+				FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.Center
+			};
+
+			Slider slider = new Slider
+			{
+				VerticalOptions = LayoutOptions.Center
+			};
+
+			label.BindingContext = slider;
+
+			label.SetBinding(Label.OpacityProperty, "Value");
+
+			Padding = new Thickness(10, 0);
+
+			Content = new StackLayout
+			{
+				Children = { label, slider }
+			};
+		}
+	}
+}
