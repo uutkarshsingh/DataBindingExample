1c7dba9c83a2eae03f0df4ddf7d63243bd48580d
utkarshSinghRathor <uutkarshsingh@gmail.com>
2016-11-14 18:34:07 +0530

    Added the code for the Data Bindings

--- DataBindingExample/DataBindingExample/Properties/OpacityBindingCodePage.xaml.cs
+++ DataBindingExample/DataBindingExample/Properties/OpacityBindingCodePage.xaml.cs
@@ -10,29 +10,7 @@ namespace DataBindingExample
 	{
 		public OpacityBindingCodePage()
 		{
-			Label label = new Label
-			{
-				Text = "Opacicty Binding Demo",
-				FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
-				VerticalOptions = LayoutOptions.CenterAndExpand,
-				HorizontalOptions = LayoutOptions.Center
-			};
-
-			Slider slider = new Slider
-			{
-				VerticalOptions = LayoutOptions.Center
-			};
-
-			label.BindingContext = slider;
-
-			label.SetBinding(Label.OpacityProperty, "Value");
-
-			Padding = new Thickness(10, 0);
-
-			Content = new StackLayout
-			{
-				Children = { label, slider }
-			};
+			InitializeComponent();
 		}
 	}
 }
