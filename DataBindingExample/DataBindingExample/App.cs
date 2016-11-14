f8bb446830a04564ecba69e532b23c9c7952c02e
utkarshSinghRathor <uutkarshsingh@gmail.com>
2016-11-14 16:15:30 +0530

    The code that runs

--- DataBindingExample/DataBindingExample/App.cs
+++ DataBindingExample/DataBindingExample/App.cs
@@ -12,20 +12,8 @@ namespace DataBindingExample
         public App()
         {
             // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
-        }
+				MainPage = new OpacityBindingCodePage();		
+		}
 
         protected override void OnStart()
         {
